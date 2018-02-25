366fb8eed265acc49376968ac97754c02c5ae1fb
Shehan Dhaleesha <sonodewcomplex@gmail.com>
2018-02-25 09:30:22 +0530

    buttons

---
+++ button.php
@@ -0,0 +1,24 @@
+<?php
+	//Button Creation
+?>
+<html>
+	<style>
+		.button {
+					background-color: #ff9800; /*orange*/
+					border: none;
+					color: white;
+					padding: 15px 32px;
+					text-align: center;
+					text-decoration: none;
+					display: inline-block;
+					font-size: 16px;
+					margin: 4px 2px;
+					cursor: pointer;
+				}
+		.button2 {background-color:#008cba;}/*blue*/
+		.button3 {background-color:#f44336;}/*red*/
+		.button4 {background-color:#e7e7e7;}/*gray*/
+		.button5 {background-color:#555555;}/*black*/
+		.button6 {background-color:#4caf50;}/*green*/
+	</style>
+</html>
